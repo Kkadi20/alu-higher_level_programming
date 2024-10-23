5adc072164b5fe088c4e31731499edd9211aa948
Kkadi20 <k.koita@alustudent.com>
2024-10-23 15:30:38 +0200

    Add 7-islower.py to check for lowercase characters

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
+
 def islower(c):
     """Check if the character c is lowercase."""
     if ord(c) > 96 and ord(c) < 123:
         return True
     return False
-
-if __name__ == "__main__":
-    # Test cases
-    test_chars = ['a', 'H', 'g', '3', 'A']
-    for char in test_chars:
-        print("{} is {}".format(char, "lower" if islower(char) else "upper"))
-
