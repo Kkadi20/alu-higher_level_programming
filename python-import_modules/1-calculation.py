6ca56be418c20b350eff8c65d4f1877a77a30223
Kkadi20 <k.koita@alustudent.com>
2024-10-23 17:46:52 +0200

    Fix calculator import script and format according to requirements

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+
+# Import the specific functions from calculator_1.py
+from calculator_1 import add, sub, mul, div
+
+# Define the variables
+a = 10
+b = 5
+
+# Use exactly 4 print functions to display the results
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+# Ensure the code is not executed when imported
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    pass
 
-    # Define variables
-    a = 10
-    b = 5
-
-    # Perform calculations and print results
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
