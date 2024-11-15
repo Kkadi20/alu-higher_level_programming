52b2faaf6107bdbe06c30f3bb304d613d1fcb562
Kkadi20 <k.koita@alustudent.com>
2024-11-15 15:15:16 +0200

    Added script to save arguments to a JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-""" Loading, adding, saving  """
-from sys import argv
+import sys
+import os
+from importlib import import_module
 
+save_to_json_file = import_module('5-save_to_json_file').save_to_json_file
+load_from_json_file = import_module('6-load_from_json_file').load_from_json_file
 
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+filename = "add_item.json"
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
 
-""" Script that adds all arguments to a Python list,
-and then save them to a file """
-try:
-    add_i = load_from_json_file('add_item.json')
-    save_to_json_file(add_i + argv[1:], 'add_item.json')
-except Exception:
-    save_to_json_file(argv[1:], 'add_item.json')
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
