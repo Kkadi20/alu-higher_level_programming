f1fc1b4d98dd943834702dc775aaad0d100c4c8f
Kkadi20 <k.koita@alustudent.com>
2024-11-16 10:31:28 +0200

    Add SQL script to count rows with id = 89

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,3 +1,2 @@
-SELECT COUNT(*) AS number
-FROM first_table
-WHERE id = 89;
+-- displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in MySQL server
+SELECT COUNT(*) FROM first_table WHERE id = 89;
