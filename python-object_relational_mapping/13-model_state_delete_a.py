3a07b288fff3179dc3693b2130329dea566e6e40
Kkadi20 <k.koita@alustudent.com>
2024-12-01 14:27:40 +0200

    Add script to delete states with 'a' in their name using SQLAlchemy

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,13 +10,24 @@ from model_state import State
 
 
 if __name__ == '__main__':
-     engine = create_engine(
+    # Create the engine with provided arguments
+    engine = create_engine(
         f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}'
     )
+    
+    # Bind the engine to a session
     InstanceSession = sessionmaker(bind=engine)
     session = InstanceSession()
+
+    # Query states where the name contains 'a' or 'A' (case-insensitive)
     states = session.query(State).filter(State.name.ilike('%a%')).all()
+
+    # Delete the selected states
     for state in states:
         session.delete(state)
+
+    # Commit changes
     session.commit()
+
+    # Close the session
     session.close()
