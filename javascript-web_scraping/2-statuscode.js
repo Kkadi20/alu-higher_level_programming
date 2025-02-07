958771de3c6660d479a67e152ab7e0fa7d4d604e
Kkadi20 <k.koita@alustudent.com>
2025-02-07 11:20:31 +0200

    Add script to display status code of a GET request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const id = process.argv[2];
-
-request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, function (err, res) {
+request(process.argv[2], function (err, res, body) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
-    console.log(JSON.parse(res.body).title);
+    console.log(`code: ${res.statusCode}`);
   }
 });
